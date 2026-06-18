8e794e91b01e9b17c4678fb63090c909dbc5da41
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-06-18 16:41:53 -0500

    Organizar paquetes y directorios

---
+++ src/main/java/io/jettra/wui/complex/SwaggerUIPage.java
@@ -0,0 +1,48 @@
+package io.jettra.wui.complex;
+
+import io.jettra.wui.core.Page;
+import java.io.IOException;
+import java.util.Map;
+
+public class SwaggerUIPage extends Page {
+
+    public static String openApiUrl = "/openapi.json"; 
+
+    public SwaggerUIPage() {
+        super("Swagger UI");
+    }
+
+    @Override
+    protected void onGet(Map<String, String> params) {
+        String resolvedUrl = com.jettra.server.JettraServer.resolvePath(openApiUrl);
+        
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"utf-8\" />\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n" +
+                "  <meta name=\"description\" content=\"SwaggerUI\" />\n" +
+                "  <title>SwaggerUI</title>\n" +
+                "  <link rel=\"stylesheet\" href=\"https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css\" />\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div id=\"swagger-ui\"></div>\n" +
+                "<script src=\"https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js\" crossorigin></script>\n" +
+                "<script>\n" +
+                "  window.onload = () => {\n" +
+                "    window.ui = SwaggerUIBundle({\n" +
+                "      url: '" + resolvedUrl + "',\n" +
+                "      dom_id: '#swagger-ui',\n" +
+                "    });\n" +
+                "  };\n" +
+                "</script>\n" +
+                "</body>\n" +
+                "</html>";
+
+        try {
+            renderResponse(currentExchange, html);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
